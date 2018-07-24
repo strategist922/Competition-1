5c1276085d636d0e7b895986505f819386ce11de
Joy <joydebmondal28@gmail.com>
2018-07-24 17:23:01 +0530

    kaggle dog vs cat with cnn

---
+++ Dogs_VS_Cats_CNN/dog_vs_cat.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 24 15:42:44 2018
+
+@author: joy
+"""
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+
+
+
+def create_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64, activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1,activation = 'sigmoid'))
+    model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    return model
+
+def create_model2():
+    # Initialising the CNN
+    model = Sequential()
+    
+    # Step 1 - Convolution
+    model.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+    
+    # Step 2 - Pooling
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+    
+    # Adding a second convolutional layer
+    model.add(Conv2D(32, (3, 3), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
+    
+    # Step 3 - Flattening
+    model.add(Flatten())
+    
+    # Step 4 - Full connection
+    model.add(Dense(units = 128, activation = 'relu'))
+    model.add(Dense(units = 1, activation = 'sigmoid'))
+    
+    # Compiling the CNN
+    model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+model = create_model2()
+model = model.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 1,
+                         validation_data = test_set,
+                         validation_steps = 2000)
+
